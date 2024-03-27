9af66b792f2307809c417ad82ca8169ce7326231
DEYYF <unaifago@gmail.com>
2024-03-27 18:29:41 +0100

    Datatable y acciones añadidas

--- src/ItemsComponents/ItemGrid.jsx
+++ src/ItemsComponents/ItemGrid.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react"
-import { Item } from "./Item";
 import { getItem } from "../helpers/getItem";
 import { Modals } from "../Components/Modals";
-import "../../styles.css"
 import { useNavigate } from "react-router-dom";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { ModalsEdits } from "../Components/ModalsEdit";
+import "../../styles.css"
 
 
 export const ItemGrid = () => {
     
     const [openModal, setOpenModal] = useState(false);
+
+    const [openModal2, setOpenModal2] = useState(false);
+
+    const [selectedItem, setSelectedItem] = useState(null)
 
     const [items, setItem] = useState(['']);
 
@@ -48,19 +54,29 @@ export const ItemGrid = () => {
         setItem(updatedItems);
     }
 
+
+
     const goBack = (event) => {
         event.preventDefault(); 
 
         navigate('/'); 
     }
 
-    const Buscador = (event) => {
-        
-        const search = event.target.value.toLowerCase();
 
-        const buscandoItem = search === '' ? copyitems : items.filter(item => item.name.toLowerCase().includes(search));
+
+    const Buscador = (event) => {
+
+        if (event.target.value.length >= 3) {
+            const search = event.target.value.toLowerCase();
+
+            const buscandoItem = items.filter(item => item.name.toLowerCase().includes(search));
         
-        setItem(buscandoItem)
+            setItem(buscandoItem)
+        }else {
+            setItem(copyitems)
+        }
+        
+        
     }
 
     
@@ -69,6 +85,27 @@ export const ItemGrid = () => {
       getitem();
       
     }, []);
+
+
+
+    const header = (
+        <div className="flex flex-wrap align-items-center justify-content-between gap-2">
+            <span className="text-xl text-900 font-bold">Items</span>
+            
+        </div>
+    );
+
+    const imageBody = (item) => {
+        return <img src={item.image} alt={item.image} className="w-6rem shadow-2 border-round"/>;
+    }
+
+    const buttonDeleteBody = (item) => {
+        return <button className="bt-eliminar" onClick={() =>deleteItem(item)}>Eliminar</button>
+    }
+
+   
+
+
 
 
     return (
@@ -83,24 +120,25 @@ export const ItemGrid = () => {
                 placeholder="Search"
                 onChange={Buscador}/>
 
-                {
-                    items.map( (item) =>(
-                        <Item
-                        key={item.id}
-                        deleteitem={deleteItem} 
-                        item={item}
-                        editItem={EditItem}
-                        />
-                    ))
-                }
+                <DataTable value={items} selectionMode="single" onSelectionChange={(e) =>{setSelectedItem(e.value);}  } header={header} tableStyle={{minWidth: '50rem'}}>
+                    
+                    <Column field="name" header="Name"></Column>
+                    <Column header="Image" body={imageBody}></Column>
+                    <Column field="species" header="Species"></Column>
+                    <Column header= "Eliminar" body={buttonDeleteBody}></Column>
+                </DataTable>
+
+                <button className="fb-more" onClick={()=>{setOpenModal(true);}}>Crear</button>
+                {openModal && <Modals setOpenModal={setOpenModal} addItem={AddItem} ultimoId={items.length}/>}
+
+                <button className="fb-edit" onClick={()=>{setOpenModal(true);}}>Editar</button>
+                {openModal && selectedItem != null && <ModalsEdits setOpenModal={setOpenModal} item={selectedItem} updateItem={EditItem}/>}
+                
 
             </div>
 
 
-            <button className="fb-more" onClick={()=>{
-        setOpenModal(true);
-      }}>Crear</button>
-      {openModal && <Modals setOpenModal={setOpenModal} addItem={AddItem} ultimoId={items.length}/>}
+            
         </>
     )
     
