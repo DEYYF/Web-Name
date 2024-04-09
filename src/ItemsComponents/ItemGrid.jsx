d675eed3bfc9e5e67bcac89e55556033519cfe02
DEYYF <unaifago@gmail.com>
2024-04-09 16:26:16 +0200

    Modificaiones musica 2

--- src/ItemsComponents/ItemGrid.jsx
+++ src/ItemsComponents/ItemGrid.jsx
@@ -148,8 +148,8 @@ export const ItemGrid = () => {
 
     useEffect(() => {
       getitem();
-        const Audio = new Audio("Rick and Morty - Evil Morty Theme Song (Feewet Trap Remix).mp3");
-        Audio.play();
+        const audio = new Audio("Rick and Morty - Evil Morty Theme Song (Feewet Trap Remix).mp3");
+        audio.play();
     }, []);
 
    
